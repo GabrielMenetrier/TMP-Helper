b24279d7bf7c7e5dfb34e17f8316524cfe8868d8
GabrielMenetrier <gabriel.r.m.santi@gmail.com>
2025-09-09 13:39:30 -0300

    2

--- utils/decisao.py
+++ utils/decisao.py
@@ -7,6 +7,5 @@ def escolher_ativos(respostas):
     Recebe as respostas do formulário e retorna uma lista de tickers da B3.
     Por enquanto, retorna uma lista fixa de exemplo.
     """
-    # TODO: Implementar lógica baseada nas respostas
-    # Exemplo: ['ITUB4.SA', 'VALE3.SA', 'WEGE3.SA']
+    print(respostas)
     return ['ITUB4.SA', 'VALE3.SA', 'WEGE3.SA']
